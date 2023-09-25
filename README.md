e7a2b722131339cd23bf38e89e8b07ea7779d4f2
cburgyan <cburgyan@gmail.com>
2023-09-25 17:13:06 -0500

    create flask/jinja pages for overview, maps, and dahsboard

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # project3
+
+## Collaborators
+
+---
+## Sources
+<ol>
+    <li>Jinja (with Flask). n.d. https://palletsprojects.com/p/jinja/</li>
+</ol>
---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify, render_template, url_for
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    
+    return render_template("index.html")
+
+
+@app.route('/maps')
+def maps():
+
+    return render_template("homesInMaps.html")
+
+
+@app.route('/Dashboard')
+def dashboard():
+
+    return render_template("dashboard.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
