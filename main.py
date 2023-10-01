97102c52fce18812c3bdfcd8e8d2f4f88df1824c
leedthanh <leedthanh@gmail.com>
2023-10-01 16:22:25 -0500

    main flask added two route for geojson

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-from flask import Flask, jsonify, render_template, url_for
-
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
+# Define a route to serve the GeoJSON file
+# if go the route it will automatically download the geojson file
+@app.route('/geojson')
+def geojson():
+    geojson_filename = 'static/DatasetManipulations/map_5rating.geojson'
+    return send_file(geojson_filename, as_attachment=True) 
+
+# adding route for 5 stars nursing home 
+@app.route('/filterMap')
+def another_geojson():
+    geojson_filename = 'static/DatasetManipulations/map.geojson'
+    return send_file(geojson_filename, as_attachment=True) 
+# Define routes for your existing pages
 @app.route('/')
 def home():
-    
     return render_template("index.html")
-
 
 @app.route('/maps')
 def maps():
-
     return render_template("homesInMaps.html")
-
 
 @app.route('/Dashboard')
 def dashboard():
-
     return render_template("dashboard.html")
-
 
 if __name__ == "__main__":
     app.run(debug=True)
+
