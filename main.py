171621307d09d69088336b333f2984969666aeef
cburgyan <cburgyan@gmail.com>
2023-10-13 10:24:12 -0500

    add other routes

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from flask import Flask, render_template, send_file, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 import os
-# import psycopg2
-
-
 
 
 list_of_truncated_nursing_dataframe_columns = ['Federal Provider Number', 'Provider Name',
@@ -28,7 +25,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
 
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False  # silence the deprecation warning
 
-# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 db = SQLAlchemy(app)
 
 
@@ -91,37 +87,6 @@ class Business(db.Model):
     )
 
 
-# # Define a route to serve the GeoJSON file
-# # if go the route it will automatically download the geojson file
-# @app.route('/bestNS')
-# def bestNS():
-#     filename = 'static/DatasetManipulations/best_nsHomes.json'
-#     return send_file(filename)
-
-# @app.route('/dashboardjson')
-# def dashboardjson():
-#     geojson_filename3 = 'static/DatasetManipulations/dashboard_tx.json'
-#     return send_file(geojson_filename3)
-
-# @app.route('/geojson')
-# def geojson():
-#     geojson_filename = 'static/DatasetManipulations/all_nursing_homes.geojson'
-#     return send_file(geojson_filename)
-
-# # adding route for 5 stars nursing home
-# @app.route('/mappingjson')
-# def another_geojson():
-#     geojson_filename2 = 'static/DatasetManipulations/mapping.json'
-#     return send_file(geojson_filename2)
-
-# # Define routes for your existing pages
-# @app.route('/')
-# def home():
-#     return render_template("index.html")
-
-# @app.route('/maps')
-# def maps():
-#     return render_template("homesInMaps.html")
 
 
 
@@ -181,39 +146,6 @@ def get_data_by_column():
     'Longitude':{},
     'Performance Id':{}
     }
-    # final_results = {
-    #     'federal_provider_number': {},
-    #     'provider_name': {},
-    #     'provider_city': {},
-    #     'provider_zip_code': {},
-    #     'provider_county_name': {},
-    #     'ownership_type': {},
-    #     'number_of_certified_beds': {},
-    #     'number_of_residents_in_certified_beds': {},
-    #     'provider_type': {},
-    #     'provider_resides_in_hospital': {},
-    #     'automatic_sprinkler_systems_in_all_required_areas': {},
-    #     'location': {},
-    #     'processing_date': {},
-    #     'latitude': {},
-    #     'longitude': {},
-    #     'adjusted_total_nurse_staffing_hours_per_resident_per_day': {},
-    #     'provider_state': {},
-    #     'provider_zip_code': {},
-    #     'most_recent_health_inspection_more_than_2_years_ago': {},
-    #     'overall_rating': {},
-    #     'health_inspection_rating': {},
-    #     'staffing_rating': {},
-    #     'rn_staffing_rating': {},
-    #     'total_weighted_health_survey_score': {},
-    #     'number_of_facility_reported_incidents': {},
-    #     'number_of_substantiated_complaints': {},
-    #     'number_of_fines': {},
-    #     'total_amount_of_fines_in_dollars': {},
-    #     'number_of_payment_denials': {},
-    #     'total_number_of_penalties': {},
-    #     'performance_id': {}
-    # }
 
     
     business_data = [
@@ -270,65 +202,6 @@ def get_data_by_column():
 
         count += 1
 
-    # print(f"final_results['Performance Id']: {final_results['Performance Id']}")
-    # for key in final_results:
-    #     for elem in list_of_truncated_nursing_dataframe_columns:
-    #         if elem.lower() == key.replace('_', ' '):
-    #             final_results = change_key_name(final_results, key, elem)
-
-    # business_data = {
-    #     'federal_provider_number': [],
-    #     'provider_name': [],
-    #     'provider_city': [],
-    #     'provider_zip_code': [],
-    #     'provider_county_name': [],
-    #     'ownership_type': [],
-    #     'number_of_certified_beds': [],
-    #     'number_of_residents_in_certified_beds': [],
-    #     'provider_type': [],
-    #     'provider_resides_in_hospital': [],
-    #     'automatic_sprinkler_systems_in_all_required_areas': [],
-    #     'location': [],
-    #     'processing_date': [],
-    #     'latitude': [],
-    #     'longitude': [],
-    #     'adjusted_total_nurse_staffing_hours_per_resident_per_day': [],
-    #     'performance_id': []
-    # }
-
-    # zipcode_data = {
-    #     'provider_state': [],
-    #     'provider_zip_code': []
-    # }
-
-    # performance_data = {
-    #     'most_recent_health_inspection_more_than_2_years_ago': [],
-    #     'overall_rating': [],
-    #     'health_inspection_rating': [],
-    #     'staffing_rating': [],
-    #     'rn_staffing_rating': [],
-    #     'total_weighted_health_survey_score': [],
-    #     'number_of_facility_reported_incidents': [],
-    #     'number_of_substantiated_complaints': [],
-    #     'number_of_fines': [],
-    #     'total_amount_of_fines_in_dollars': [],
-    #     'number_of_payment_denials': [],
-    #     'total_number_of_penalties': [],
-    #     'performance_id': []
-    # }
-
-    # for business, zipcode, performance in results:
-    #     for key in business_data:
-    #         business_data[key].append(getattr(business, key))
-    #     for key in zipcode_data:
-    #         zipcode_data[key].append(getattr(zipcode, key))
-    #     for key in performance_data:
-    #         performance_data[key].append(getattr(performance, key))
-
-    # combined_data = {**business_data, **zipcode_data, **performance_data}
-
-    # # Convert combined_data to a list of dictionaries, where each dictionary represents a column
-    # dataByColumn = [{"columnName": key, "values": value} for key, value in combined_data.items()]
 
     return jsonify(final_results)
 
@@ -396,8 +269,41 @@ def get_data_by_row():
 
     return jsonify(data)
 
+# Define a route to serve the GeoJSON file
+# if go the route it will automatically download the geojson file
+@app.route('/bestNS')
+def bestNS():
+    filename = 'static/DatasetManipulations/best_nsHomes.json'
+    return send_file(filename)
 
-@app.route("/")
+# Not operational
+# @app.route('/dashboardjson')
+# def dashboardjson():
+#     geojson_filename3 = 'static/DatasetManipulations/dashboard_tx.json'
+#     return send_file(geojson_filename3)
+
+# Download Nursing Home Data
+@app.route('/geojson')
+def geojson():
+    geojson_filename = 'static/DatasetManipulations/all_nursing_homes.geojson'
+    return send_file(geojson_filename)
+
+# adding route for 5 stars nursing home
+@app.route('/mappingjson')
+def another_geojson():
+    geojson_filename2 = 'static/DatasetManipulations/mapping.json'
+    return send_file(geojson_filename2)
+
+# Define routes for your existing pages
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/maps')
+def maps():
+    return render_template("homesInMaps.html")
+
+@app.route("/Dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
